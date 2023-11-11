b965b4532d34348b9bada16089d1383bc1e31a4e
kpate402 <kushpatel19876@gmail.com>
2023-11-10 20:35:52 -0600

    Team Mascot

--- src/screens/Settings/Settings.js
+++ src/screens/Settings/Settings.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {Text, View, StatusBar, StyleSheet} from 'react-native';
+import {Text, View, StatusBar, StyleSheet, Image} from 'react-native';
+import Pikachu from '../../images/drPikachu.png';
 
 const Settings = ({navigation}) => {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor={'#353535'} />
       <Text style={styles.text}>Settings</Text>
+      <View>
+        <Image style={{width: 300, height: 300}} source={Pikachu} />
+      </View>
+      <Text style={styles.text}>Pika Pika Settings not ready yet Pika Pika</Text>
     </View>
   );
 };
